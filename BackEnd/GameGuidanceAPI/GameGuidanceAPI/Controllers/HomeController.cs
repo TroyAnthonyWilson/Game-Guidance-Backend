a76502eb002138c12cfe2e634183898d8215469f
Jude Davis <juducation@outlook.com>
2022-11-28 22:27:12 -0500

    Updates 11/28
    -Updated the HomeController
    -Updated the GameGuidanceDbContext
    -Updated User Model
    -Added Question and Variables Models

--- BackEnd/GameGuidanceAPI/GameGuidanceAPI/Controllers/HomeController.cs
+++ BackEnd/GameGuidanceAPI/GameGuidanceAPI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
+using GameGuidanceAPI.Models;
 using RestSharp;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -25,10 +26,35 @@ namespace GameGuidanceAPI.Controllers
             request.AddHeader("Authorization", "Bearer 1w3wtuaj6g10l2zttajubqwveonvtf");
             request.AddHeader("Content-Type", "text/plain");
             request.AddHeader("Cookie", "__cf_bm=tArho0gINIfLmN3bLfKD9VmJJXO_zA0icrIpJZwzsdE-1669564263-0-AeA4CPYMcXk+VQzXR0z36LHOrx7xkYr8hr49f/zZZ6EaAcL7B2S7ufy5ixCu/2kMQOqyzps9Vmqx9Y+kWCWKPL0=");
-            var body = @"fields *; where id=740;";
+            var body = @"fields *;";
             request.AddParameter("text/plain", body, ParameterType.RequestBody);
             RestResponse response = client.Execute(request);
             return response.Content;
         }
     }
+
+    // POST Request to return recommended game
+    // POST api/<HomeController>
+
+    //[HttpPost]
+    //public string PostRecommend([FromBody] string value)
+    //{
+    //    var client = new RestClient("https://api.igdb.com/v4/games");
+    //    RestClientOptions options = new RestClientOptions("https://api.igdb.com/v4/games")
+    //    {
+    //        ThrowOnAnyError = true,
+    //        MaxTimeout = -1
+    //    };
+    //    var request = new RestRequest("https://api.igdb.com/v4/games", Method.Post);
+    //    request.AddHeader("Client-ID", "n9kcwb4ynvskjy7bd147jk94tdt6yw");
+    //    request.AddHeader("Authorization", "Bearer 1w3wtuaj6g10l2zttajubqwveonvtf");
+    //    request.AddHeader("Content-Type", "text/plain");
+    //    request.AddHeader("Cookie", "__cf_bm=tArho0gINIfLmN3bLfKD9VmJJXO_zA0icrIpJZwzsdE-1669564263-0-AeA4CPYMcXk+VQzXR0z36LHOrx7xkYr8hr49f/zZZ6EaAcL7B2S7ufy5ixCu/2kMQOqyzps9Vmqx9Y+kWCWKPL0=");
+    //    var body = @$"{Variables.recommendRequest}";
+    //    request.AddParameter("text/plain", body, ParameterType.RequestBody);
+    //    RestResponse response = client.Execute(request);
+    //    return response.Content;
+    //}
+
+
 }
---
+++ BackEnd/GameGuidanceAPI/GameGuidanceAPI/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Identity.Client;
+using GameGuidanceAPI.Models;
+
+namespace GameGuidanceAPI.Models
+{
+    public class Question
+    {
+        public bool SinglePlayer { get; set; }
+        public bool MultiPlayer { get; set; } 
+        public bool CoOp { get; set; }
+        public bool Versus { get; set; }
+        public bool Standalone { get; set; }
+        public bool Series { get; set; }
+        public bool FirstPerson { get; set; }
+        public bool ThirdPerson { get; set; }
+        public double MinimumRating { get; set; }
+        public string? Genre { get; set; }
+        public string[]? Theme { get; set; }
+        public string[]? Keywords { get; set; }
+        public List<string>? Parameters { get; set; }
+
+    }
+}
