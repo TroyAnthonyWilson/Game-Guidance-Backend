633ce4ac79485bd40ec1f0ffb2dc43248e63093d
Troy Wilson <wilson.troy.anthony@gmail.com>
2022-12-01 21:00:18 -0500

    Here we go

--- BackEnd/GameGuidanceAPI/GameGuidanceAPI/Controllers/UserFavoriteController.cs
+++ BackEnd/GameGuidanceAPI/GameGuidanceAPI/Controllers/UserFavoriteController.cs
@@ -1,4 +1,5 @@
-﻿using GameGuidanceAPI.Models;
+﻿using GameGuidanceAPI.Context;
+using GameGuidanceAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -8,12 +9,19 @@ namespace GameGuidanceAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserFavorite : ControllerBase
+    public class UserFavoriteController : ControllerBase
     {
 
+        private readonly GameGuidanceDBContext _authContext;
+
+        public UserFavoriteController(GameGuidanceDBContext gameGuidanceDBContext)
+        {
+            _authContext = gameGuidanceDBContext;
+        }
+
         // POST api/<HomeController>
-        [HttpPost]
-        public string Post([FromBody] string value)
+        [HttpPost("addfavorite")]
+        public async string Post(int userId, int gameId)
         {
             var client = new RestClient("https://api.igdb.com/v4/games");
             RestClientOptions options = new RestClientOptions("https://api.igdb.com/v4/games")
@@ -26,14 +34,23 @@ namespace GameGuidanceAPI.Controllers
             request.AddHeader("Authorization", "Bearer 1w3wtuaj6g10l2zttajubqwveonvtf");
             request.AddHeader("Content-Type", "text/plain");
             request.AddHeader("Cookie", "__cf_bm=tArho0gINIfLmN3bLfKD9VmJJXO_zA0icrIpJZwzsdE-1669564263-0-AeA4CPYMcXk+VQzXR0z36LHOrx7xkYr8hr49f/zZZ6EaAcL7B2S7ufy5ixCu/2kMQOqyzps9Vmqx9Y+kWCWKPL0=");
-            var body = @"fields *;where id = 40104;";
+            var body = @"fields *;where id = " + gameId +";";
             request.AddParameter("text/plain", body, ParameterType.RequestBody);
             RestResponse response = client.Execute(request);
 
             //deserialize the first response
-       
-
+      
             List<JsonDeserializer> myDeserializedClass = JsonConvert.DeserializeObject<List<JsonDeserializer>>(response.Content);
+
+            //add game with matching id to user favorites in the database
+            UserFavorite userFavorite = new UserFavorite {
+                UserId = userId,
+                GameId = myDeserializedClass[0].id
+            };
+
+            //save to userFavorite to database
+            await _authContext.UserFavorites.AddAsync(userFavorite);
+            await _authContext.SaveChangesAsync();
 
             return JsonConvert.SerializeObject(myDeserializedClass);
 
--- BackEnd/GameGuidanceAPI/GameGuidanceAPI/Models/UserFavorite.cs
+++ BackEnd/GameGuidanceAPI/GameGuidanceAPI/Models/UserFavorite.cs
@@ -1,6 +1,16 @@
-﻿namespace GameGuidanceAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameGuidanceAPI.Models
 {
     public class UserFavorite
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+
+        public int GameId { get; set; }
     }
 }
