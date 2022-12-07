ed9a45ddb241c0e1d2c228093687c78ef8ec1cf7
Elber Funez <elberfunez@gmail.com>
2022-12-06 22:28:32 -0500

    backend method for final post moved to answer controller

--- BackEnd/GameGuidanceAPI/GameGuidanceAPI/Controllers/AnswerController.cs
+++ BackEnd/GameGuidanceAPI/GameGuidanceAPI/Controllers/AnswerController.cs
@@ -43,7 +43,32 @@ namespace GameGuidanceAPI.Controllers
             return newAnswer;
         }
 
-        [HttpPost("ChangeAnswer")]
+        // POST api/<HomeController>
+        [HttpPost("FinalPost")]
+        public async Task<string> FinalPost([FromBody] Answer answer)
+        {
+            var client = new RestClient("https://api.igdb.com/v4/games");
+            RestClientOptions options = new RestClientOptions("https://api.igdb.com/v4/games")
+            {
+                ThrowOnAnyError = true,
+                MaxTimeout = -1
+            };
+            var request = new RestRequest("https://api.igdb.com/v4/games", Method.Post);
+            request.AddHeader("Client-ID", "n9kcwb4ynvskjy7bd147jk94tdt6yw");
+            request.AddHeader("Authorization", "Bearer 1w3wtuaj6g10l2zttajubqwveonvtf");
+            request.AddHeader("Content-Type", "text/plain");
+            request.AddHeader("Cookie", "__cf_bm=tArho0gINIfLmN3bLfKD9VmJJXO_zA0icrIpJZwzsdE-1669564263-0-AeA4CPYMcXk+VQzXR0z36LHOrx7xkYr8hr49f/zZZ6EaAcL7B2S7ufy5ixCu/2kMQOqyzps9Vmqx9Y+kWCWKPL0=");
+            var body = $"fields name; limit 1; where game_modes=({answer.GameMode}); where genres=({answer.Genre}) & " +
+                $"player_perspectives=({answer.PlayerPerspective}) & themes=({answer.Theme}) & category=(0,8,9,11); & status=0 & " +
+                $"where platforms=({answer.Platform});";
+            request.AddParameter("text/plain", body, ParameterType.RequestBody);
+            RestResponse response = client.Execute(request);
+            Console.WriteLine(body);
+            return response.Content;
+        }
+    
+
+    [HttpPost("ChangeAnswer")]
         public async Task<ActionResult<Answer>> ChangeAnswer(Answer answer)
         {
             if (!ModelState.IsValid)
